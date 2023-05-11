cb44d29d85d12ca3ea813b506471387571b78904
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-05-11 21:58:09 +0200

    Crear estructura componentes

---
+++ src/app/modules/recepies/recepies.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecepiesRoutingModule } from './recepies-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecepiesRoutingModule
+  ]
+})
+export class RecepiesModule { }
