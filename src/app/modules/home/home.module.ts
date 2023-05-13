38f5544798c139a2913c3accaca1267f879876d5
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-05-13 15:29:40 +0200

    Vinculacion HomePage componente al modulo Home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
