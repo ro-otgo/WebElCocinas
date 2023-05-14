c196bebf0cc309beeebf98889c3fe8640603af7f
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-05-14 14:20:40 +0200

    App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'recipes',
+    loadChildren:() => import('./modules/recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  {
+    path: 'login',
+    loadChildren:() => import('./modules/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'signup',
+    loadChildren:() => import('./modules/sign-up/sign-up.module').then(m => m.SignUpModule)
   }
 ];
 
