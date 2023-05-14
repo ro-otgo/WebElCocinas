258a05ad9222cb8a63508fae243c091a4f186800
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-05-14 14:20:51 +0200

    Recipes routing

--- src/app/modules/recipes/recipes-routing.module.ts
+++ src/app/modules/recipes/recipes-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailRecipesComponent } from './pages/detail-recipes/detail-recipes.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipesComponent
+  },
+  {
+    path: 'detail',
+    component: DetailRecipesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
