03e59da6b3ba1120c20421886a33d761eebc7681
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-05-14 14:09:29 +0200

    Recipes and detail recipes componentes

--- src/app/modules/recipes/recipes.module.ts
+++ src/app/modules/recipes/recipes.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { DetailRecipesComponent } from './pages/detail-recipes/detail-recipes.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RecipesComponent,
+    DetailRecipesComponent
+  ],
   imports: [
     CommonModule,
     RecipesRoutingModule
