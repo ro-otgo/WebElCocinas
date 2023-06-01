96509cf9fec04e482d315c58ab18ce642bc227de
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-06-01 23:40:33 +0200

    Modificacion modelos

--- src/app/core/models/Recipe.ts
+++ src/app/core/models/Recipe.ts
@@ -1,10 +1,6 @@
-import { Ingredient } from "./Ingredient";
-
 export interface Recipe {
   id: Number,
   image: string,
   title: string,
-  ingredients?: Array<Ingredient>,
-  recipeText: string
 }
 
