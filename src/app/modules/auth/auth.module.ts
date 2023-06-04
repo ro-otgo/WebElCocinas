2e9f72a7d49ae26b792219bf2de068e12c1458ec
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-06-04 11:14:39 +0200

    Crear login y singup
    Se crea los componentes login y singup en el modulo auth.
    Solo el html

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
