9ab17d4b03a32d60962a121c1a6d83c0b75b58a7
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-06-04 11:52:53 +0200

    Rutas crear receta

--- src/app/modules/recipes/recipes-routing.module.ts
+++ src/app/modules/recipes/recipes-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailRecipesComponent } from './pages/detail-recipes/detail-recipes.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
+import { CreateRecipeComponent } from './pages/create-recipe/create-recipe.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,11 @@ const routes: Routes = [
   {
     path: 'detail/:id',
     component: DetailRecipesComponent
-  }
+  },
+  {
+    path: 'create',
+    component: CreateRecipeComponent
+  },
 ];
 
 @NgModule({
