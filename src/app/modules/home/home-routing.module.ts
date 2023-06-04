ec849fb02ea0bc5c4dc4614396cab9d86694a202
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2023-06-04 16:52:05 +0200

    Eliminar codigo innecesario de home component

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -4,13 +4,9 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path: 'recipes',
-    loadChildren:() => import('@modules/recipes/recipes.module').then(m => m.RecipesModule)
-  },
-  {
-    path: 'signout',
-    loadChildren:() => import('@modules/signout/signout.module').then(m => m.SignoutModule)
-  },
+    path:'',
+    component: HomePageComponent
+  }
 ];
 
 @NgModule({
